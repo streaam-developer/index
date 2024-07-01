0a8dc4566241f9dc2709b528c20e9962e31cad1d
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2024-07-01 22:00:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -8,5 +8,5 @@ ADMINS = [5764988016, 6206665581]
 FORWARD_CHAT = int("-1001918999034")
 DOMAIN = "https://test.test/"
 DATABASE_URL = "mongodb+srv://fehebaw351:nHbjrujWqgqLR58H@cluster0.lekn97z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DATABASE_NAME = "Cluster0"
+DATABASE_NAME = "cluster0"
 FLOG_CHANNEL = int("-1001918999034")
