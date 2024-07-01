f4eca911599f2ad90855b64f8285647d16f7f927
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2024-07-01 21:41:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,12 +1,12 @@
 
 #bot deploy mandetory info
-BOT_TOKEN = "6936728011:AAH6FV7SJDEiG-u_uIGG6fkj3L2FvF-gRpY"
+BOT_TOKEN = "6936728011:AAEf1Thv1fFLv_yvXMqWYyEyO2Am2oHZjjQ"
 API_ID = int("24010108")
 API_HASH = "8d89700b2fc09a3aa6c906cbed65b040"
 
 ADMINS = [5764988016, 6206665581]
-FORWARD_CHAT = int("-1002025279155")
+FORWARD_CHAT = int("-1001918999034")
 DOMAIN = "https://test.test/"
 DATABASE_URL = "mongodb+srv://sonu:19040456@cluster0.hwk6ivg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "Cluster0"
-FLOG_CHANNEL = int("-1002119584175")
+FLOG_CHANNEL = int("-1001918999034")
